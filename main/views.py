afc3bb95fba2e7fa520781c0ed241eff58c85a10
Malvin Scafi <154596305+Cyades@users.noreply.github.com>
2024-10-23 11:14:14 +0700

    fix navbar and css

--- main/views.py
+++ main/views.py
@@ -15,6 +15,7 @@ def search_recipes(request):
     filter_type = request.GET.get('none_query') or request.GET.get('ingredient_query') or request.GET.get('name_query')
 
     if filter_type == 'none':
+        print("0")
         recipes = Recipe.objects.filter(
             Q(recipe_name__icontains=query) |
             Q(ingredients__name__icontains=query) |
@@ -23,10 +24,13 @@ def search_recipes(request):
             Q(recipe_instructions__description__icontains=query)
         ).distinct()
     elif filter_type == 'name':
+        print("1")
         recipes = Recipe.objects.filter(recipe_name__icontains=query).distinct()
     elif filter_type == 'ingredient':
+        print("2")
         recipes = Recipe.objects.filter(ingredients__name__icontains=query).distinct()
     else:
+        print("3")
         recipes = Recipe.objects.filter(
             Q(recipe_name__icontains=query) | 
             Q(ingredients__name__icontains=query) |
