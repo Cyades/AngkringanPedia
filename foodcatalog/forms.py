d5d72f51394ef73dc90d65758de68361f629a7ea
nobeljb <nobeljulianbintang@gmail.com>
2024-10-25 19:05:21 +0700

    sedikit memperbaiki tampilan

--- foodcatalog/forms.py
+++ foodcatalog/forms.py
@@ -11,3 +11,12 @@ class RatingReviewForm(forms.ModelForm):
     def clean_score(self):
         score = self.cleaned_data.get('score')
         return int(score)  # Pastikan untuk mengembalikan nilai sebagai integer
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['score'].widget.attrs.update({
+            'class': 'mt-1 border rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-[#6B8E23] w-full'
+        })
+        self.fields['content'].widget.attrs.update({
+            'class': 'mt-1 border rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-[#6B8E23] w-full' 
+        })
