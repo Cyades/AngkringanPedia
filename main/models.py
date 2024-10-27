352af11e5c9a45299bf9b8187e9f0bca6ee9aadf
Juan <juanlukius1512@gmail.com>
2024-10-27 20:30:18 +0700

    cadangan sebelum ngepull

--- main/models.py
+++ main/models.py
@@ -14,6 +14,7 @@ class Recipe(models.Model):
     image_url = models.URLField(max_length=500)
     ingredients = models.ManyToManyField(Ingredient, related_name="recipes")
     instructions = models.TextField()
+    
 
     def __str__(self):
         return self.recipe_name
--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from main.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from favorites.views import get_makanan_favorite, create_favorite, delete_favorite, cek_favorit
 
 app_name = 'main'
 
@@ -15,7 +16,11 @@ urlpatterns = [
     path('adminku/', show_admin, name='show_admin'),
     path('adminkudelete/<int:id>', delete_user, name='delete_user'),
     path('get_user_details/<int:user_id>/', get_user_details, name='get_user_details'),
-    path('edit-admin/<int:id>', edit_admin, name='edit_admin')
+    path('edit-admin/<int:id>', edit_admin, name='edit_admin'),
+    path('favorites/<int:id_user>/', get_makanan_favorite, name = 'favorites'),
+    path('favorites/delete/<int:id_user>/<int:id_makanan>/',delete_favorite, name = 'delete_favorites'),
+    path('favorites/create/<int:id_user>/<int:id_makanan>/', create_favorite, name = 'create_favorites'),
+    path('favorites/cek_favorit/<int:id_user>/<int:id_makanan>/', cek_favorit, name = 'cek_favorit')
 ]
 
 if settings.DEBUG:
