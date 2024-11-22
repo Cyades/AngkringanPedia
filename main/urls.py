3d96aaca2c95b1a68d293002c0c955243bb83a50
Malvin Scafi <154596305+Cyades@users.noreply.github.com>
2024-11-23 05:42:33 +0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -10,13 +10,6 @@ urlpatterns = [
     path('search/', search_recipes, name='search_recipes'),
     path('add-recipe/', add_recipe, name='add_recipe'),
     path('delete/<int:id>', delete_product, name='delete_product'),
-    path('register/', register, name='register'),
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
-    path('adminku/', show_admin, name='show_admin'),
-    path('adminkudelete/<int:id>', delete_user, name='delete_user'),
-    path('get_user_details/<int:user_id>/', get_user_details, name='get_user_details'),
-    path('edit-admin/<int:id>', edit_admin, name='edit_admin')
 ]
 
 if settings.DEBUG:
