d49da9a09b93696f1b7ce5f5aa9f00c26b2b20b5
kaachiii <kaachi579@gmail.com>
2024-12-22 10:38:33 +0700

    add cors headers

--- AngkringanPedia/settings.py
+++ AngkringanPedia/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'favorites',
     'artikel',
     'dashboard',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
